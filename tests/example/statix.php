b6c00bc95e2727cca711a9bcd47339c34ceef37d
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2021-12-29 06:26:13 -0800

    wip

--- tests/example/statix.php
+++ tests/example/statix.php
@@ -1,9 +1,6 @@
 <?php
 
 use Statix\Application;
-use Symfony\Component\Finder\Finder;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
 require_once './../../vendor/autoload.php';
 
 $app = Application::new();
